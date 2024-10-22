01d970f8a488b0f41bb02507d76f628d1305124a
Yassine Elmassaoudi <yassine.elmassaoudi_ext@payback.net>
2024-10-22 21:32:59 +0100

    switch back

--- src/redux/modal/saga.ts
+++ src/redux/modal/saga.ts
@@ -1,4 +1,4 @@
-import { call, delay, Effect, fork, put, spawn, takeEvery, type ForkEffect } from 'redux-saga/effects';
+import { call, Effect, put, takeEvery, type ForkEffect } from 'redux-saga/effects';
 import { modalActions, ModalTypes } from './slice';
 import { PayloadAction } from '@reduxjs/toolkit';
 import { UIGraphNode } from '../graph/slice';
@@ -10,17 +10,11 @@ export function* dismissModal(): Generator<Effect, void> {
 }
 
 export function* openNodeActionModal(action: PayloadAction<UIGraphNode>): Generator<Effect, void> {
-  // yield call(openModal, ModalTypes.NodeActionModal, { 'node': action.payload })
-  yield put(modalActions.setBag({ 'node': action.payload }))
-  yield put(modalActions.setChosenModal(ModalTypes.NodeActionModal))
-  yield put(modalActions.setIsPortalOpen(true))
+  yield call(openModal, ModalTypes.NodeActionModal, { 'node': action.payload })
 }
 
 export function* openAddNodeModal(action: PayloadAction<UIGraphNode>): Generator<Effect, void> {
-  // yield call(openModal, ModalTypes.AddNodeModal, { 'node': action.payload })
-  yield put(modalActions.setBag({ 'node': action.payload }))
-  yield put(modalActions.setChosenModal(ModalTypes.AddNodeModal))
-  yield put(modalActions.setIsPortalOpen(true))
+  yield call(openModal, ModalTypes.AddNodeModal, { 'node': action.payload })
 }
 
 function* openModal(type: ModalTypes, bag: Record<string, any>): Generator<Effect, void> {
